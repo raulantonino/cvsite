406c84d9374cd6abd1a062c684f31efffcc1cb0c
Antonino <antoninooh@gmail.com>
2026-03-30 09:51:27 -0300

    chore: prepare production dependencies and static settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,17 +1,53 @@
+import os
+
 from .base import *  # noqa: F401,F403
 from .base import _env_list
 
 
 DEBUG = False
 ALLOWED_HOSTS = _env_list('ALLOWED_HOSTS', [])
+CSRF_TRUSTED_ORIGINS = _env_list('CSRF_TRUSTED_ORIGINS', [])
 
+postgres_env_vars = ('DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST')
+postgres_configured = all(os.environ.get(name) for name in postgres_env_vars)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if postgres_configured:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASSWORD'],
+            'HOST': os.environ['DB_HOST'],
+            'PORT': os.environ.get('DB_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / os.environ.get('SQLITE_NAME', 'db.sqlite3'),
+        }
+    }
+
+
+WHITENOISE_ENABLED = False
+
+try:
+    import whitenoise  # noqa: F401
+except ImportError:
+    pass
+else:
+    WHITENOISE_ENABLED = True
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+    STORAGES = {
+        'default': {
+            'BACKEND': 'django.core.files.storage.FileSystemStorage',
+        },
+        'staticfiles': {
+            'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        },
+    }
 
 
 SESSION_COOKIE_SECURE = True
